4044d5fd83bb2d7c55e55ef4f8ef84f037224409
kyleb <kyleb3nko@gmail.com>
2019-07-19 18:40:53 +0200

    tried to be fancy and failed

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import tf from '@tensorflow/tfjs'
+import tfvis from '@tensorflow/tfjs-vis'
+
+// Get Car data
+// Reduce to variables we are interested in
+// Clean missing data
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello TensorFLow</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
